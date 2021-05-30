55b90f1e8ecd57d482dc58fed77d7e3662ed9c24
Porosyatka <narbgo@yandex.ru>
2021-05-30 20:45:34 +0300

    part2 game

--- lol001.py
+++ lol001.py
@@ -2,7 +2,7 @@
 import pygame
 import random
 
-WIDTH = 360  # ширина игрового окна
+WIDTH = 500  # ширина игрового окна
 HEIGHT = 480  # высота игрового окна
 FPS = 30  # частота кадров в секунду
 
@@ -14,12 +14,29 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        self.rect.x += 5
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+
 # создаем игру и окно
 pygame.init()
 pygame.mixer.init()  # для звука
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 # Цикл игры
 running = True
@@ -32,9 +49,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     # Обновление
-
+    all_sprites.update()
     # Рендеринг
     screen.fill(BLACK)
+    all_sprites.draw(screen)
     # после отрисовки всего, переворачиваем экран
     pygame.display.flip()
 
