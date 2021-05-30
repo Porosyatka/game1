8b92eb57098f4624417c10665fcbd47bcb2c3535
Porosyatka <narbgo@yandex.ru>
2021-05-30 19:30:36 +0300

    opop

---
+++ lol001.py
@@ -0,0 +1,30 @@
+# Pygame шаблон - скелет для нового проекта Pygame
+import pygame
+import random
+
+WIDTH = 360  # ширина игрового окна
+HEIGHT = 480  # высота игрового окна
+FPS = 30  # частота кадров в секунду
+
+# Цвета (R, G, B)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Рендеринг
+screen.fill(BLACK)
+# создаем игру и окно
+pygame.init()
+pygame.mixer.init()  # для звука
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+# Цикл игры
+running = True
+while running:
+    # ввод процесса (события)
+    # Обновление
+    # Визуализация (сборка)
