a614b73171c5f4df2044c6c22c556e247158a6e1
Porosyatka <narbgo@yandex.ru>
2021-05-31 14:54:32 +0300

    add img

--- lol001.py
+++ lol001.py
@@ -13,6 +13,8 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+speed1 = 3
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -23,9 +25,26 @@ class Player(pygame.sprite.Sprite):
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
 
     def update(self):
-        self.rect.x += 5
+        self.rect.x += 3
         if self.rect.left > WIDTH:
             self.rect.right = 0
+
+
+class Player2(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        self.rect.y += 3
+        if self.rect.bottom >= HEIGHT:
+            self.rect.y -= 10
+
+            #self.rect.bottom = 0
 
 
 # создаем игру и окно
@@ -36,7 +55,9 @@ pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 player = Player()
+player2 = Player2()
 all_sprites.add(player)
+all_sprites.add(player2)
 
 # Цикл игры
 running = True
